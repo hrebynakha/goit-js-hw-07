9fc03b5948e72b7f6cb8207ca37da9582ff1ba20
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-09-15 21:48:26 +0300

    add hw 3-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const widgetSpan = document.querySelector(".color");
+changeColorBtn.addEventListener("click", handleClickChgColor);
+
+function handleClickChgColor(e) {
+  const color = getRandomHexColor();
+  widgetSpan.textContent = color;
+  body.style.backgroundColor = color;
+}
